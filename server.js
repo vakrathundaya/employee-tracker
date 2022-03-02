1fb203c6e661cf053bf1a34f229ba4d910cc4354
Radhika <cu9rad@gmail.com>
2022-03-02 14:11:33 -0500

    user prompt added

--- server.js
+++ server.js
@@ -13,3 +13,33 @@ connection.connect(function (err) {
     if (err) throw err;
     init();
 });
+
+const promtUser = () => {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices:
+                ['View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add a Department',
+                    'Add a Role',
+                    'Add an Employee',
+                    'Update an Employee Role',
+                    'Update an Employee Manager',
+                    'View Employees By Department',
+                    'Delete a Department',
+                    'Delete a Role',
+                    'Delete an Employee',
+                    'View Department Budget',
+                    'Exit']
+
+        }
+
+    ]).then((answers) => {
+        const{choices} = answers;
+    })
+}
